5850b2ffe617cfddc0bff1feb000974499f13148
Doka900 <doki.janjic@gmail.com>
2022-05-03 13:54:57 +0200

    handleri i graceful

--- model.go
+++ model.go
@@ -2,4 +2,8 @@ package main
 
 type Config struct {
 	entries map[string]string
+	Id      string   `json:"id"`
+	Title   string   `json:"title"`
+	Text    string   `json:"text"`
+	Tags    []string `json:"tags"`
 }
--- server.go
+++ server.go
@@ -1,5 +1,70 @@
 package main
 
+import (
+	"errors"
+	"mime"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 type Service struct {
-	data map[string][]*Config
+	data map[string]*Config
+}
+
+func (ts *Service) createPostHandler(w http.ResponseWriter, req *http.Request) {
+	contentType := req.Header.Get("Content-Type")
+	mediatype, _, err := mime.ParseMediaType(contentType)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if mediatype != "application/json" {
+		err := errors.New("Expect application/json Content-Type")
+		http.Error(w, err.Error(), http.StatusUnsupportedMediaType)
+		return
+	}
+
+	rt, err := decodeBody(req.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	id := createId()
+	rt.Id = id
+	ts.data[id] = rt
+	renderJSON(w, rt)
+}
+
+func (ts *Service) getAllHandler(w http.ResponseWriter, req *http.Request) {
+	allTasks := []*Config{}
+	for _, v := range ts.data {
+		allTasks = append(allTasks, v)
+	}
+
+	renderJSON(w, allTasks)
+}
+
+func (ts *Service) getPostHandler(w http.ResponseWriter, req *http.Request) {
+	id := mux.Vars(req)["id"]
+	task, ok := ts.data[id]
+	if !ok {
+		err := errors.New("key not found")
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	renderJSON(w, task)
+}
+
+func (ts *Service) delPostHandler(w http.ResponseWriter, req *http.Request) {
+	id := mux.Vars(req)["id"]
+	if v, ok := ts.data[id]; ok {
+		delete(ts.data, id)
+		renderJSON(w, v)
+	} else {
+		err := errors.New("key not found")
+		http.Error(w, err.Error(), http.StatusNotFound)
+	}
 }
